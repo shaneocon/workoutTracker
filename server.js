b5752c7e7657a91c9a83e997d636001e8c46fd44
shaneocon <shane.pat.oconnell@gmail.com>
2021-02-07 18:03:46 -0800

    added server.js page

--- server.js
+++ server.js
@@ -2,3 +2,25 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path")
 const mongoose = require("mongoose");
+
+const PORT = process.envPORT || 8080;
+
+const db = require("./models");
+
+const app = express();
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+
+mongoose.connect(process.env.MONGODB_URI || 
+  "mongodb://localhost/nosql", { useNewUrlParser: true, useUnifiedTopology: true});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
