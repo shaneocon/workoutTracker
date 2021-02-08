e983a750769eb55e4860d106bb59b41c6be59e1c
shaneocon <shane.pat.oconnell@gmail.com>
2021-02-07 20:29:41 -0800

    added html routes

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path")
-const mongoose = require("mongoose");
+const seed = require("./seeders/seed");
 
 const PORT = process.envPORT || 8080;
-
-const db = require("./models");
 
 const app = express();
 
@@ -18,8 +14,8 @@ app.use(express.static("public"));
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-mongoose.connect(process.env.MONGODB_URI || 
-  "mongodb://localhost/nosql", { useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI ||
+  "mongodb://localhost/nosql", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
