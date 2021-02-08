02bcd791e07b6d9d399341b24e090302632e3925
shaneocon <shane.pat.oconnell@gmail.com>
2021-02-07 23:23:39 -0800

    added first get route to apiRoutes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,15 @@
-const db = require("./models");
+const db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+      console.log(1);
+    })
+    .catch(err => {
+      res.json(err);
+      console.log(2);
+    })
+  })
+}
