d16c6302b17c64fe1f3ee4176a67a286091c0f07
shaneocon <shane.pat.oconnell@gmail.com>
2021-02-09 15:20:52 -0800

    changed localhost to workout in seed

--- seeders/seed.js
+++ seeders/seed.js
@@ -2,7 +2,7 @@ let mongoose = require("mongoose");
 let db = require("../models");
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/',
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
